4d56939d1454379823b811a21e3c3a64fd474a62
Stephanie Wilson <steffi.nicole.wilson@gmail.com>
2015-03-03 21:43:35 -0800

    Added exercises 1-5

---
+++ ex5.py
@@ -0,0 +1,18 @@
+my_name = 'Stephanie Wilson'
+my_age = 24
+my_height = 62
+my_weight = 95
+my_eyes = 'Blue'
+my_teeth = 'Whitish'
+my_hair = "Reddish blonde"
+
+print "Let's talk about %s." % my_name
+print "She is %d inches tall." % my_height
+print "She's %d pounds heavy." % my_weight
+print "That's actually really light."
+print "She has %s eyes and %s hair." % (my_eyes, my_hair)
+print "Her teeth are usually %s depending on the coffee." % my_teeth
+
+print "If I add %d, %d, and %d I get %d." % (my_age, my_height, my_weight, my_age + my_height + my_weight)
+
+print "Yes, you can concatenate a decimal variable like %d and a string variable like %s into your string." % (my_age, my_eyes)
